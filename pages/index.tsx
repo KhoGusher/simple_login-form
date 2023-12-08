23b5daeb31c5c864dbafb49e122fd12a1ca2ccd9
KhoGusher <kondwaninantchito@gmail.com>
2023-12-08 17:01:52 +0200

    changed vercel look on form

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
+    <div>
      <Signin />
-    </main>
+    </div>
   )
 }
